7d2d817307ee524668cfd64bae1b39293bc59109
fumiya_kohno <fumiya_kohno@forcia.com>
2016-11-16 08:14:52 +0900

    update

---
+++ src/actionLamda/re.hs
@@ -0,0 +1,36 @@
+import Control.Applicative
+import System.Random
+
+replicateM' 0 _ = return []
+replicateM' n a | n > 0 = (:) <$> a <*> replicateM' (n - 1) a
+
+replicateM_' 0 _ = return []
+replicateM_' n a | n > 0 = a >> replicateM_' (n - 1) a
+
+
+
+main = do
+    let dice = getStdRandom $ randomR (1, 6) :: IO Int
+    print =<< replicateM' 5 dice
+    putStrLn "---"
+    replicateM_' 3 $ do
+        print =<< dice
+    putStrLn "---"
+    a <- forM' [1..3] $ \i -> do
+        print i
+        return i
+    print a
+    putStrLn "---"
+    forM_' [1..3] $ \i -> do
+        print i
+    putStrLn "---"
+    let y f = f (y f)
+    y $ \f -> do
+        r <- dice
+        print r
+        when' (r /= 1) f
+    putStrLn "---"
+    y $ \f -> do
+        r <- dice
+        print r
+        unless' (r == 6) f
